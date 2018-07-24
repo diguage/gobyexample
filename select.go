d27786ed53aa5823d52a8733d14fa4dfe85b8212
diguage <leejun119@gmail.com>
2018-07-24 23:20:49 +0800

    add error code

---
+++ select.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "math/rand"
+import "time"
+
+func main() {
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	go func() {
+		time.Sleep(rand.Int31n(5) * time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(rand.Int31n(5) * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+
+}
