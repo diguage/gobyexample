7240ab3146fbac742a1202f88164f39ff56ab501
diguage <leejun119@gmail.com>
2018-08-01 21:44:49 +0800

    specify a number of loop

--- atomic-counters.go
+++ atomic-counters.go
@@ -10,13 +10,13 @@ func main() {
 
 	for i := 0; i < 50; i++ {
 		go func() {
-			for {
+			for j := 0; j < 1000; j++ {
 				atomic.AddUint64(&ops, 1)
 				time.Sleep(time.Millisecond)
 			}
 		}()
 	}
-	time.Sleep(time.Second)
+	time.Sleep(5 * time.Second)
 
 	opsFinal := atomic.LoadUint64(&ops)
 	fmt.Println("ops:", opsFinal)
