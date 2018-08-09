fab9dae9e9b9549319a979ca39d77279bc11c76a
diguage <leejun119@gmail.com>
2018-08-09 13:30:19 +0800

    uncomplete defer

---
+++ defer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.Fi)
